2ce702107f71e0b9ccd1bc3dd61bacf165ff4329
py01scc <gatien.clerc@eduvaud.ch>
2026-02-11 13:33:00 +0100

    feat: street color

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -17,51 +17,70 @@ function draw() {
     //draw square
     // corner left top
     stroke('white');
-    fill ('white');
-    rect (1011,36,100,100);
+    fill('#242424');
+    rect(1011, 36, 100, 100);
 
     //corner right top
-    stroke('white');
-    fill ('white');
-    rect (1784,36,100,100);
+    rect(1784, 36, 100, 100);
 
     //corner left bottom
-    stroke('white');
-    fill ('white');
-    rect (1011,809,100,100);
+    rect(1011, 809, 100, 100);
 
     //corner left bottom
-    stroke('white');
-    fill ('white');
-    rect (1784,809,100,100);
+    rect(1784, 809, 100, 100);
 
-    //square top
-    for (let i=0; i < 9; i++){
-        stroke('red');
-        fill ('green');
-        rect (1111 + (i * 75), 36, 75, 100);
+    //square
+    for (let i = 0; i < 9; i++) {
+        let color_top = ['#de1c1c', null, '#de1c1c', '#de1c1c', null, '#e8ee3a', '#e8ee3a', null, '#e8ee3a'];
+        let color_right = ['#14a14a', '#14a14a', null, '#14a14a', null, null, '#3982e4', null, '#3982e4'];
+        let color_bottom = [''];
+        let color_left = ['white', 'black'];
+
+        fill('#242424');
+        //square top
+        rect(1111 + (i * 75), 36, 75, 100);
+
+        //square right
+        rect(1784, 136 + (i * 75), 100, 75);
+
+        //square bottom
+        rect(1111 + (i * 75), 809, 75, 100);
+
+        //square left
+        rect(1011, 136 + (i * 75), 100, 75);
+
+        //street top color
+        stroke('white');
+        fill('red');
+        rect(1111 + (i * 75), 110, 75, 26);
+
+        //street right color
+        stroke('white');
+        fill('red');
+        rect(1784 , 136 + (i * 75), 26, 75);
+
+        //street bottom color
+        stroke('white');
+        fill('red');
+        rect(1111 + (i * 75) , 809 , 75,  26);
+
+        //street left color
+        stroke('white');
+        fill('red');
+        rect(1086, 136 +  (i * 75), 26, 75);
+
     }
-
-    //square right
-    for (let i=0; i < 9; i++){
-        stroke('red');
-        fill ('green');
-        rect (1784, 136 + (i * 75), 100, 75);
-    }
-
-    //square bottom
-    for (let i=0; i < 9; i++){
-        stroke('red');
-        fill ('green');
-        rect (1709 - (i * 75) ,809,75,100);
-    }
-
-    //square left
-    for (let i=0; i < 9; i++){
-        stroke('red');
-        fill ('green');
-        rect (1011, 136 + (i * 75), 100, 75);
-    }
-
 
 }
+
+/*
+*  if (i === 0 || i === 2 || i === 3 || i === 5 || i === 6 || i === 8) {
+            if (i === 0 || i === 2 || i === 3) {
+                // couleur rouge
+            }
+            else {
+                // couleur jaune
+            }
+
+        }
+* */

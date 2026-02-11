f4e44ca20bc33cca1123b67b01577d6d3b7154e7
py01scc <gatien.clerc@eduvaud.ch>
2026-02-11 11:40:16 +0100

    feat: sqare rect

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,9 +1,67 @@
 "use strict";
 function setup() {
-    createCanvas(1910, 1070);
-    background();
+    createCanvas(windowWidth, windowHeight);
+    background('blue');
 }
 
 function draw() {
-    // ici tu mettras ton code d’animation
+    //draw game board
+    // location (1011,36,   1886,36,   1011,911,    1886,911)
+    let widthRect = 875;
+    let heightRect = 875;
+
+    noStroke();
+    fill('#242424')
+    rect(width - widthRect - 35, height / 2 - heightRect / 2, widthRect, heightRect);
+
+    //draw square
+    // corner left top
+    stroke('white');
+    fill ('white');
+    rect (1011,36,100,100);
+
+    //corner right top
+    stroke('white');
+    fill ('white');
+    rect (1784,36,100,100);
+
+    //corner left bottom
+    stroke('white');
+    fill ('white');
+    rect (1011,809,100,100);
+
+    //corner left bottom
+    stroke('white');
+    fill ('white');
+    rect (1784,809,100,100);
+
+    //square top
+    for (let i=0; i < 9; i++){
+        stroke('red');
+        fill ('green');
+        rect (1111 + (i * 75), 36, 75, 100);
+    }
+
+    //square right
+    for (let i=0; i < 9; i++){
+        stroke('red');
+        fill ('green');
+        rect (1784, 136 + (i * 75), 100, 75);
+    }
+
+    //square bottom
+    for (let i=0; i < 9; i++){
+        stroke('red');
+        fill ('green');
+        rect (1709 - (i * 75) ,809,75,100);
+    }
+
+    //square left
+    for (let i=0; i < 9; i++){
+        stroke('red');
+        fill ('green');
+        rect (1011, 136 + (i * 75), 100, 75);
+    }
+
+
 }

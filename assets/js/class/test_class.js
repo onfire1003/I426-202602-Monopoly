ae5065c728f4b98a1044166217a295fbc6fcd17d
CedricJankiewicz <pm14cau@eduvaud.ch>
2026-03-10 14:44:02 +0100

    feat:turn function in class game

--- assets/js/class/test_class.js
+++ assets/js/class/test_class.js
@@ -1,7 +1,8 @@
 "use strict"
 import Game from "./game.js";
 
-//test game innit
+//-----test game innit-----
+console.log("//////////////Innit//////////////");
 let game = new Game(4);
 console.log(game.nb_player);
 console.log(game.players);
@@ -9,6 +10,7 @@ console.log(game.board_size);
 console.log(game.board);
 
 //-----test player-----
+console.log("//////////////Player//////////////");
 //money
 game.players[0].addMoney(1000);
 console.log(game.players[0]);
@@ -30,3 +32,16 @@ game.players[3].putInPrison()
 console.log(game.players[3]);
 game.players[3].releaseFromPrison()
 console.log(game.players[3]);
+
+//-----test turn-----
+console.log("//////////////Turn//////////////");
+console.log(game.players[game.current_player]);
+game.finishTurn()
+console.log(game.players[game.current_player]);
+console.log(game.possible_actions);
+game.throwTheDice()
+game.buy()
+game.sell()
+game.build()
+game.trade(2)
+game.goOutOfPrison()
